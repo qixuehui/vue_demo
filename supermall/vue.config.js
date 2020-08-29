3b5f0723e79624ed530b408f0b1ee00adef97b07
qxh <3048086150@qq.com>
2020-08-29 11:31:48 +0800

    css文件的引入 base.css的书写 配置路径相关配置 配置统一风格

--- supermall/vue.config.js
+++ supermall/vue.config.js
@@ -1,6 +1,9 @@
 module.exports = {
     configureWebpack: {
+        //路径相关的配置
         resolve: {
+            //扩展名
+            extensions: [],
             //别名
             alias: {
                 'components': '@/components',
